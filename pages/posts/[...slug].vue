b9be0f824517eb0aadf0d965bb1ab5c3975e6243
Skorikov Kirill <skorikovkr@gmail.com>
2024-03-14 17:49:30 +0500

    Post page

---
+++ pages/posts/[...slug].vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+const route = useRoute();
+
+const { data, pending } = await useLaravelFetch(`/api/posts/${route.params.slug}`);
+
+</script>
+
+<template>
+  <div class="index-page">
+    <div 
+      v-if="!pending"
+      class="post-container" 
+    >
+      <h1>{{ (data as any).data.title }}</h1>
+    </div>
+    <div v-else>
+      Загрузка...
+    </div>
+  </div>
+</template>
+
+<style></style>
